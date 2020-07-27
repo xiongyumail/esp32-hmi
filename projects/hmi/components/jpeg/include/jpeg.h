0e9954b05fc41fe5619da552c125922bd0bd1b2b
xiongyu <xiongyu@espressif.com>
2020-07-27 20:22:28 +0800

    add jpeg encode

--- projects/hmi/components/jpeg/include/jpeg.h
+++ projects/hmi/components/jpeg/include/jpeg.h
@@ -2,7 +2,10 @@
 #include <stdint.h>
 #include "esp_err.h"
 #include "tjpgd.h"
+#include "jpegenc.h"
 
 #define JPEG_WORK_BUF_SIZE 3100
 
 uint8_t *jpeg_decode(uint8_t *jpeg, int *w, int* h);
+
+size_t jpeg_encode(uint8_t *img, int w, int h, uint8_t *jpeg, size_t max_size);
